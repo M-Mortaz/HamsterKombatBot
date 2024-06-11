2c5bf7f7ba4928304e23fc2abb69c113e598e0ea
Mehdi <Mortaz.Mehdi@gmail.com>
2024-06-11 14:34:34 +0330

    Addd optional config

--- bot/config/config.py
+++ bot/config/config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,26 +9,26 @@ class Settings(BaseSettings):
     API_ID: int
     API_HASH: str
 
-    MIN_AVAILABLE_ENERGY: int = 100
-    SLEEP_BY_MIN_ENERGY: list[int] = [1800, 2400]
+    MIN_AVAILABLE_ENERGY: Optional[int] = 100
+    SLEEP_BY_MIN_ENERGY: Optional[list[int]] = [1800, 2400]
 
-    ADD_TAPS_ON_TURBO: int = 2500
+    ADD_TAPS_ON_TURBO: Optional[int] = 2500
 
-    AUTO_UPGRADE: bool = True
-    MAX_LEVEL: int = 20
+    AUTO_UPGRADE: Optional[bool] = True
+    MAX_LEVEL: Optional[int] = 20
 
-    BALANCE_TO_SAVE: int = 1000000
-    UPGRADES_COUNT: int = 10
+    BALANCE_TO_SAVE: Optional[int] = 1000000
+    UPGRADES_COUNT: Optional[int] = 10
 
-    APPLY_DAILY_ENERGY: bool = True
-    APPLY_DAILY_TURBO: bool = True
+    APPLY_DAILY_ENERGY: Optional[bool] = True
+    APPLY_DAILY_TURBO: Optional[bool] = True
 
-    RANDOM_TAPS_COUNT: list[int] = [10, 50]
-    SLEEP_BETWEEN_TAP: list[int] = [10, 25]
+    RANDOM_TAPS_COUNT: Optional[list[int]] = [10, 50]
+    SLEEP_BETWEEN_TAP: Optional[list[int]] = [1, 3]
 
-    USE_RANDOM_USERAGENT: bool = False
+    USE_RANDOM_USERAGENT: Optional[bool] = False
 
-    USE_PROXY_FROM_FILE: bool = False
+    USE_PROXY_FROM_FILE: Optional[bool] = False
 
 
 settings = Settings()
